fe6a1114187f793e51bcad4d36984acface3f962
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-01-16 19:25:56 +0000

    slider woes

--- page-templates/blocks/lc_clients.php
+++ page-templates/blocks/lc_clients.php
@@ -1,51 +1,61 @@
 <?php
 $classes = $block['className'] ?? null;
 ?>
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+<style>
+.clients .clients__slide {
+    display: grid;
+    place-content: center;
+    height: 60px;
+}
+.clients .clients__slide img {
+    max-height: 60px;
+    max-width: 200px;
+    filter: brightness(5) grayscale(1) brightness(50%);
+}
+</style>
 <section class="clients <?=$classes?>">
     <div class="clients__inner">
-        <div class="container-xl">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
+            <div class="clients__slider">
                 <?php
                 while (have_rows('client_logos','options')) {
                     the_row();
                     ?>
-                    <div class="swiper-slide"><img src="<?=wp_get_attachment_image_url(get_sub_field('logo'),'full')?> " alt="" width="100%"></div>
+                    <div class="clients__slide"><img src="<?=wp_get_attachment_image_url(get_sub_field('logo'),'full')?> " alt="" width="100%"></div>
                     <?php
                 }
                 ?>
-                </div>
-            </div>
         </div>
-    </div>
+    </div>    
 </section>
 
 <?php
 add_action('wp_footer',function(){
     ?>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script>
-const swiper = new Swiper('.swiper-container', {
-  speed: 6000,
-  spaceBetween: 10,
-  slidesPerView: 3,
-  loop: true,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  },
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 40,
-//     },
-//     992: {
-//       slidesPerView: 4,
-//       spaceBetween: 50,
-//     },
-//   }
-});
+(function($) {
+    $(document).ready(function(){
+        $('.clients__slider').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                }
+            ]
+        });
+    });
+}(jQuery))
 </script>
     <?php
-});
+},9999);
