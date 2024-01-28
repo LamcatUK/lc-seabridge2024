d0207a8406b691cbef5c8eda492211b19c50c3b0
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-01-28 10:54:45 +0000

    fixed header/footer overlap

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,22 +1,35 @@
 // Add your custom JS here.
 
 /* Function to add styles after page load */
-jQuery(function($){
-    var navListItems = document.querySelectorAll('ul.nav-list li');
-console.log('here'+navListItems);
-    navListItems.forEach((item, index) => {
-        var style = document.createElement('style');
-        document.head.appendChild(style);
-        style.sheet.insertRule(`ul.nav-list li:nth-child(${index + 1}) { view-transition-name: navLink-${index + 1}; }`, 0);
-    });
-},9999
-);
+// outputting empty <style> tags at the moment...
+// jQuery(function($){
+//     var navListItems = document.querySelectorAll('ul.nav-list li');
+// console.log('here'+navListItems);
+//     navListItems.forEach((item, index) => {
+//         var style = document.createElement('style');
+//         document.head.appendChild(style);
+//         style.sheet.insertRule(`ul.nav-list li:nth-child(${index + 1}) { view-transition-name: navLink-${index + 1}; }`, 0);
+//     });
+// },9999
+// );
 
 
 AOS.init({
     once: true,
     easing: 'ease-in',
 });
+
+window.onscroll = function() {
+    if (window.scrollY > 800) {
+        document.querySelectorAll('.hero').forEach(element => {
+            element.style.zIndex = '-999';
+        });
+    } else {
+        document.querySelectorAll('.hero').forEach(element => {
+            element.style.zIndex = '-1';
+        });
+    }
+};
 
 // jQuery(function($){
 //     var btn = $('#to-top');
